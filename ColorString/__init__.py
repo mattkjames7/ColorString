7fba80582b986706dc520d49e2ece6fc2ff2aaf5
mattkjames7 <mattkjames7@gmail.com>
2018-12-12 15:44:08 +0000

    fixed error in init

--- ColorString/__init__.py
+++ ColorString/__init__.py
@@ -1,1 +1,1 @@
-from .ColorString import ColorString,GetBGCode,GetEndFormat,GetFGCode,_IntToBGColor,IntToFGColor,_RGBtoColor,_StringToBGColor,_StringToFGColor
+from .ColorString import ColorString,GetBGCode,GetEndFormat,GetFGCode,_IntToBGColor,_IntToFGColor,_RGBtoColor,_StringToBGColor,_StringToFGColor
